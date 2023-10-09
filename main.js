3f2a57833903cb6d8602ad7a50aac7807f05196b
HannahMcCabe31 <hmccabe.hm@gmail.com>
2023-10-09 10:29:15 +0100

    attempt at adding location input

--- main.js
+++ main.js
@@ -1,26 +1,36 @@
 // Asynchronously retrieves the weather and displays it
-async function getAndDisplayWeather() {
-    const currentWeather = await retrieveWeather();
-    displayWeather(currentWeather);
-  }
-  // Function to asynchronously retrieve the current London from an API
-async function retrieveWeather() {
-    // Send a GET request to the weather API. Await the response.
-    // Declare a variable to store the HTTP response
-    const response = await fetch("https://api.open-meteo.com/v1/forecast?latitude=51.5085&longitude=-0.1257&current_weather=true", {
-      headers: {
-        Accept: "application/json",
-      },
-    });
-// Check if the response failed, and if so log an error and halt the app
-if (!response.ok) {
-    console.error(`Status: ${response.status}`);
-    console.error(`Text: ${await response.text()}`);
-    return;
-  }
-  // return the parsed JSON from the response (which contains the weather object)
-  const data = await response.json();
-  return data;
+async function getAndDisplayWeather(location) {
+    const currentWeather = await retrieveWeather(location);
+   displayWeather(currentWeather);
+}
+
+// Function to asynchronously retrieve the weather from an API
+async function retrieveWeather(location) {
+    // Modify the API URL to include the user's chosen location
+    const apiUrl = `https://api.open-meteo.com/v1/forecast?location=${location}&current_weather=true`;
+
+    try {
+        // Send a GET request to the weather API. Await the response.
+        // Declare a variable to store the HTTP response
+        const response = await fetch(apiUrl, {
+            headers: {
+                Accept: "application/json",
+            },
+        });
+
+        // Check if the response failed
+        if (!response.ok) {
+            console.error(`Status: ${response.status}`);
+            console.error(`Text: ${await response.text()}`);
+            return;
+        }
+
+        // Try to parse the response as JSON
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Error fetching or parsing data:", error);
+    }
 }
 
 // Function to update the DOM with the provided weather
@@ -78,7 +88,19 @@ function displayWeather(currentWeather) {
   }
 // Waits for the DOM to be fully loaded and then displays an initial weather.
 
-document.addEventListener("DOMContentLoaded", getAndDisplayWeather);
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.getElementById("get-weather-button");
+
+    // Add an event listener to the button
+  button.addEventListener("click", () => {
+    const locationInput = document.getElementById("location-input").value;
+    console.log(locationInput)
+        getAndDisplayWeather(locationInput);
+    });
+
+    // Display initial weather on page load
+    getAndDisplayWeather("");
+});
 
 
 
